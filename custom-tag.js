a25a6a699d00be002851612fc2f9d76aa36c14bc
Andrea Giammarchi <andrea.giammarchi@mailonline.co.uk>
2017-01-18 20:23:02 +0000

    added tests

--- custom-tag.js
+++ custom-tag.js
@@ -1,16 +1,18 @@
+/*! (C) 2017 Andrea Giammarchi - Mit Style License */
+
 function CustomTag(options) { return CustomTag.define(options); }
 
 CustomTag.get = customElements.get.bind(CustomTag);
 
 CustomTag.whenDefined = customElements.whenDefined.bind(CustomTag);
 
-CustomTag.define = function (options) {
+CustomTag.define = function (options) {'use strict';
   var
     defineProperty = Object.defineProperty,
-    extend = options.extends,
+    statics = options['static'] || (options['static'] = {}),
+    extend = options['extends'],
     extendString = typeof extend === 'string',
     extendNative = extendString && extend.indexOf('-') < 0,
-    statics = options.static || (options.static = {}),
     name = options.name,
     watch = options.watch,
     onAdopt = options.onAdopt,
@@ -20,66 +22,64 @@ CustomTag.define = function (options) {
     onInit = options.onInit,
     hasInit = !!onInit,
     hasConnect = !!onConnect,
-    WeakInit = hasInit ? new WeakSet() : null,
-    init = hasInit ?
-      (self) => {
-        if (!WeakInit.has(self)) {
-          WeakInit.add(self);
-          onInit.call(self);
-        }
-      } :
-      Object,
     Super = extend ?
       (extendString ?
         (extendNative ?
           document.createElement(extend).constructor :
           customElements.get(extend)
-        ) :
-        extend
-      ) :
-      HTMLElement,
-    Class
+        ) : extend
+      ) : HTMLElement,
+    Component,
+    WeakInit
   ;
+  if (hasInit) {
+    WeakInit = typeof WeakSet === 'function' ?
+      new WeakSet :
+      {
+        add: function (el) { el.__CustomTag = {}; },
+        has: function (el) { return el.__CustomTag; }
+      }
+  }
   options['extends'] = Super;
-  if (onAdopt) {
-    options.adoptedCallback = onAdopt;
-  }
-  if (onChange) {
-    options.attributeChangedCallback = onChange;
-  }
-  if (onDisconnect) {
-    options.disconnectedCallback = onDisconnect;
-  }
+  if (onAdopt) options.adoptedCallback = onAdopt;
+  if (onChange) options.attributeChangedCallback = onChange;
+  if (onDisconnect) options.disconnectedCallback = onDisconnect;
   options.connectedCallback = function () {
-    if (hasInit) init(this);
+    if (hasInit && !WeakInit.has(this)) {
+      WeakInit.add(this);
+      this.onInit();
+    }
     if (hasConnect) this.onConnect.apply(this, arguments);
   };
   if (watch) {
     defineProperty(statics, 'observedAttributes', {
-      enumerable: true,
-      get: function () {
-        return watch;
-      }
+      configurable: true,
+      get: function () { return watch; }
     });
     watch.forEach(function (key) {
       if (!options.hasOwnProperty(key)) {
-        defineProperty(options, key, {
+        defineProperty(options, key.replace(
+          /-([a-z])/g,
+          function ($0, $1) { return $1.toUpperCase(); }
+        ), {
           configurable: true,
-          enumerable: true,
           get: function get() { return this.getAttribute(key); },
           set: function set(value) { this.setAttribute(key, value); }
         });
       }
     });
   }
-  Class = CustomTag.Class(options);
+  Component = (CustomTag.Class || Class)(options);
   customElements.define.apply(
     customElements,
     extendNative ?
-      [name, Class, {extends: extend}] :
-      [name, Class]
+      [name, Component, {'extends': extend}] :
+      [name, Component]
   );
-  return Class;
+  return Component;
 };
 
-try { module.exports = CustomTag; } catch(meh) {}
+try {
+  module.exports = CustomTag;
+  CustomTag.Class = require('classtrophobic');
+} catch(meh) {}
---
+++ test.js
@@ -0,0 +1,78 @@
+this.onload = function () {
+
+  var test = tressa;
+
+  alert('Results are in console');
+
+  test.title('CustomTag');
+
+  test.async(function (done) {
+    var MyElement;
+
+    CustomTag.whenDefined('my-element').then(function () {
+      test.log('## MyElement');
+      test(CustomTag.get('my-element') === MyElement, 'class');
+      document.body.appendChild(new MyElement);
+    });
+
+    MyElement = CustomTag({
+      name: 'my-element',
+      watch: ['attribute'],
+      onInit: function () {
+        test(true, 'initialized');
+        this.textContent = this.nodeName;
+      },
+      onConnect: function () {
+        test(true, 'connected');
+        this.attribute = 'test';
+      },
+      onChange: function (name, prev, curr) {
+        test(name === 'attribute', 'changed');
+        test(prev == null, 'previous value');
+        test(curr === 'test', 'current value');
+        this.parentNode.removeChild(this);
+      },
+      onDisconnect: function () {
+        test(true, 'disconnected');
+        done();
+      }
+    });
+
+  });
+
+  test.async(function (done) {
+    var MyButton;
+
+    CustomTag.whenDefined('my-button').then(function () {
+      test.log('## MyButton');
+      test(CustomTag.get('my-button') === MyButton, 'class');
+      document.body.appendChild(new MyButton);
+    });
+
+    MyButton = CustomTag({
+      name: 'my-button',
+      extends: 'button',
+      watch: ['attri-bute'],
+      onInit: function () {
+        test(true, 'initialized');
+        this.textContent = this.nodeName;
+      },
+      onConnect: function () {
+        test(true, 'connected');
+        this.attriBute = 'test';
+      },
+      onChange: function (name, prev, curr) {
+        test(name === 'attri-bute', 'changed');
+        test(prev == null, 'previous value');
+        test(curr === 'test', 'current value');
+        this.parentNode.removeChild(this);
+      },
+      onDisconnect: function () {
+        test(true, 'disconnected');
+        done();
+      }
+    });
+
+  });
+
+};
